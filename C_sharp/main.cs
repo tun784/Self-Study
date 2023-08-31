b15c7fc3c92e289b6e9fbeba03c61fbf5746a9c7
tun784 <thudientu782004@gmail.com>
2023-08-31 16:10:00 +0700

    August 31st

--- C_sharp/main.cs
+++ C_sharp/main.cs
@@ -4,46 +4,38 @@ using System;
 
 namespace Yooo
 {
-    internal class program
+    internal class Program
     {
-        static void Main(string[] args)
-        {
+        static void Main(string[] args){
             int year = 0;
             Console.Write("Please enter a year: ");
             //year = int.Parse(Console.ReadLine());
-            if (int.TryParse(Console.ReadLine(), out year))
-            {
+            if (int.TryParse(Console.ReadLine(), out year)){
                 if (!((year % 4 == 0 && year % 100 != 0) || year % 400 == 0)) //Chia hết cho 4 nhưng không chia hết cho 100 hoặc chia hết cho 400
                 {
                     Console.WriteLine("{0} isn't a leap year.", year);
                 }
-                else
-                {
+                else{
                     Console.WriteLine("{0} is a leap year.", year);
                 }
             }
-            else
-            {
+            else{
                 Console.WriteLine("Invalid input. Please enter a valid year.");
             }
 
             double point=0;
             Console.Write("Enter your point: ");
-            //point = double.Parse(Console.ReadLine());
-            //if (double.TryParse(Console.ReadLine(), NumberStyles.Any, CultureInfo.InvariantCulture, out point)
-            if (double.TryParse(Console.ReadLine(), out point))
-            {
-                if (point >= 5)
-                {
+            // point = double.Parse(Console.ReadLine());
+            // if (double.TryParse(Console.ReadLine(), NumberStyles.Any, CultureInfo.InvariantCulture, out point)
+            if (double.TryParse(Console.ReadLine(), out point)){
+                if (point >= 5){
                     Console.WriteLine("Point: {0} ==> 'Pass the exam'.", point);
                 }
-                else
-                {
+                else{
                     Console.WriteLine("Point: {0} ==> 'Failed the exam'.", point);
                 }
             }
-            else
-            {
+            else{
                 Console.WriteLine("Invalid input. Please enter a valid numeric value.");
             }
         }
