1216d0f9fa1c03b38987152ec3a82dd037df83fb
tun784 <thudientu782004@gmail.com>
2023-10-02 00:12:36 +0700

    02 October, 2023 - 00:12

--- Python/test03.py
+++ Python/test03.py
@@ -1,4 +1,5 @@
 import sys
+import os
 import math
 import pyttsx3
 from datetime import datetime, date
@@ -8,7 +9,8 @@ robot_input = speech_recognition.Recognizer()
 robot_output = pyttsx3.init()
 robot_AI = "Unknown"
 user = "..."
-
+question = ""
+result = ""
 def inputFunction():
     user = input("Ask me anything: ")
     return user
@@ -20,19 +22,19 @@ def outputFunction(answer):
 def AI_Function(prompt):
     if prompt == "":
         robot_AI = "I can't hear what did you say, try again."
-    elif "hello" in prompt:
-        robot_AI = "Hello guys"
-    elif "today" in prompt:
+    elif "hello" in prompt or "hi" in prompt or "lo" in prompt:
+        robot_AI = "Hello guys."
+    elif "today" in prompt or "day" in prompt:
         today = date.today()
         robot_AI = today.strftime("%B %d, %Y")
-    elif "time" in prompt:
+    elif "time" in prompt or "now" in prompt:
         now = datetime.now()
         robot_AI = now.strftime("%H hours %M minutes %S seconds")
-    elif "name" in prompt:
-        robot_AI = "The name of owner's code is Nguyen Hoang Tuan"
-    elif prompt == "exit":
-        print("Exiting the program")
-        outputFunction()
+    elif "name" in prompt or "owner" in prompt or "code" in prompt:
+        robot_AI = "The name of owner's code is Nguyen Hoang Tuan."
+    elif prompt == "exit" or prompt == "exit the program":
+        robot_AI = "Exiting the program..."
+        outputFunction(robot_AI)
         sys.exit()
     else:
         robot_AI = "I don't understand what did you say, try again."
@@ -41,8 +43,6 @@ def AI_Function(prompt):
 
 def main():
     while 1==1:
-        user = input("Ask me anything: ")
-        
         # print("Do you want to use micro or keyboard ?")
         # option = int(input("1/0: "))
         # if option == 1:
@@ -58,8 +58,10 @@ def main():
         #         user = "*Invalid sounds*"]        
         #print("User: " + user)
         question = inputFunction()
-        AI_Function(question)
-        outputFunction()
+        result = AI_Function(question)
+        outputFunction(result)
         
+    os.system("cls")
+
 if __name__ == "__main__":
     main()
