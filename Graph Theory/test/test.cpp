8247c9c9d7c6431ac7b57449be4c1b5f74235d33
tun784 <thudientu782004@gmail.com>
2024-03-17 07:50:40 +0700

    17-03-2024 07:50

--- Graph Theory/test/test.cpp
+++ Graph Theory/test/test.cpp
@@ -10,13 +10,20 @@ struct Graph
     int m[MAX][MAX];
 };
 
+Graph khoi_tao(int n) {
+    Graph g;
+    // Initialize the graph here
+    return g;
+}
+
 Graph Doc_ma_tran_ke(string fileName)
 {
     Graph g;
     ifstream fileIn;
 
     fileIn.open(fileName, ios::in);
-    if (fileIn.fail()){
+    if (fileIn.fail())
+    {
         cout << "File khong ton tai";
         g.flag = -1;
         return g;
@@ -33,7 +40,45 @@ Graph Doc_ma_tran_ke(string fileName)
     return g;
 }
 
+Graph Doc_danh_sach_canh(string fileName)
+{
+    Graph g;
+
+    ifstream fileIn;
+    fileIn.open(fileName, ios::in);
+    if (fileIn.fail())
+    {
+        cout << "File khong ton tai !!!\n";
+        g.flag = -1;
+        return g;
+    }
+
+    int flag, n;
+    fileIn >> flag;
+    fileIn >> n;
+    g = khoi_tao(n);
+    g.flag = flag;
+    
+    int so_canh;
+    fileIn >> so_canh;
+
+    for (int i = 0; i < so_canh; i++)
+    {
+        int dinh_dau, dinh_cuoi;
+        fileIn >> dinh_dau;
+        fileIn >> dinh_cuoi;
+
+        g.m[dinh_dau][dinh_cuoi] = 1;
+        if (g.flag == 0)
+            g.m[dinh_cuoi][dinh_dau] = 1;
+    }
+
+    fileIn.close();
+    return g;
+}
+
 int main()
 {
+    
     return 0;
 }
