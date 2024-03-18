f79bdf80cd9bd7d40de2d568d316bd95b7e462e3
tun784 <thudientu782004@gmail.com>
2024-03-18 17:37:21 +0700

    18-03-2024 17:37

--- Java/Hello.java
+++ Java/Hello.java
@@ -1,9 +1,46 @@
+import java.util.Scanner;
+
 public class Hello {
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
-		int numA = 5;
-		int numB = 10;
-		float numC = (float)numA / numB;
-		System.out.println(numC);
+		int numA = 5, numB = 10;
+		int numC = numA + numB;
+		int numD = numA - numB;
+		int numE = numB * numA;
+		double numF = (double)numA / numB;
+		System.out.println(numA + " and " + numB + ": " + numC + " " + numD + " " + numE + " " + numF);
+
+
+		int age = 18;
+		if (age >= 18) {
+			System.out.println("You are an adult");
+		}
+		else{
+			System.out.println("You are a child");
+		}
+
+		int i;
+		for (i = 0; i < 5; i++){
+			System.out.print(i + "-");
+		}
+		System.out.println("");
+
+		i = 0;
+		while (i < 10){
+			System.out.print(i + "_");
+			i++;
+		}
+		System.out.println("");
+
+		i = 0;
+		do {
+			System.out.print(i + " ");
+			i++;
+		} while (i < 15);
+		System.out.println("");
+
+		Scanner scan = new Scanner(System.in);
+		System.out.print("What's your name: ");
+		String name = scan.nextLine();
+		System.out.println("Hello " + name + " !");
 	}
 }
--- JavaScript/app.js
+++ JavaScript/app.js
@@ -1,2 +1,75 @@
-var msg = 'Hello World';
-console.log(msg);
+// Hello world
+
+/* Hello
+        World */
+
+var message = 'Hello World';
+function sayHello(){
+    console.log(message);
+}
+sayHello();
+
+var a = 5, b = 10;
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+console.log((a / b).toFixed(2)); // Fix: Use toFixed() method to limit decimal places to 2
+
+var year = new Date().getFullYear();
+var nextYear = year + 1;
+var isLeapYear = false;
+isLeapYear = year % 4 == 0 && (year % 100 != 0 || year % 400 == 0);
+if (isLeapYear) {
+    console.log(year + ' is a leap year');
+}
+else {
+    console.log(year + ' is not a leap year');
+}
+
+isLeapYear = nextYear % 4 == 0 && (nextYear % 100 != 0 || nextYear % 400 == 0);
+if (isLeapYear)
+    console.log(nextYear + ' is a leap year');
+else
+    console.log(nextYear + ' is not a leap year');
+
+var age = 18;
+if (age >= 18)
+    console.log('You are an adult');
+else
+    console.log('You are a child');
+
+var i = 0;
+let output = '';
+for (i = 0; i < 5 ; i++) {
+    output += i + '-';
+}
+console.log(output);
+
+i = 0;
+output = '';
+while (i < 10) {
+    output += i + '_';
+    i++;
+}
+console.log(output);
+
+i = 0;
+output = '';
+do {
+    output += i + ' ';
+    i++;
+}
+while (i < 15);
+console.log(output);
+
+console.log(0.1 + 0.2);
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+readline.question("What's your name: ", (userName) => {
+    console.log("Hello " + userName);
+    readline.close();
+});
